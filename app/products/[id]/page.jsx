97708bb8d6318c2a32c331f16fa39380279e2f97
spandin <willstesi@gmail.com>
2023-06-13 22:24:31 +0300

    prod page and anim bg

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -8,35 +8,37 @@ import { IcButton } from "@/components/IcButton/IcButton";
 import { MdEdit, MdDelete } from "react-icons/md";
 
 import { db } from "@/lib/firebase";
+import { doc, onSnapshot } from "firebase/firestore";
 
-import { doc, query, onSnapshot } from "firebase/firestore";
-
-export default async function Product({ params }) {
-  const [product, setProduct] = useState([]);
+export default function Product({ params }) {
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
-    const q = doc(db, "products", `${params.id}`);
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let productArr = [];
-
-      querySnapshot.forEach((doc) => {
-        productArr.push({ ...doc.data(), id: doc.id });
-      });
-
-      setProduct(productArr);
-    });
+    const unsubscribe = onSnapshot(
+      doc(db, "products", `${params.id}`),
+      (doc) => {
+        let data = doc.data();
+        console.log(data);
+        setProduct(data);
+      }
+    );
     return () => unsubscribe();
-  });
+  }, [params.id]);
 
   return (
     <div className="Product min-w-sceen flex w-full flex-col">
       <div className="Product__info flex flex-col bg-green-100 p-5 lg:rounded-t-lg">
-        <h1></h1>
-        <p className="text-[12px]">Дата добавления: 00.00.0000</p>
+        <h1>
+          {product.name} - {product.quantity} шт.
+        </h1>
+        <p className="text-[12px]">Дата добавления: {product.dateAdded}</p>
       </div>
 
-      <div className="Product__body flex basis-full bg-darkD-300 p-5">
-        My Post{" "}
+      <div className="Product__body flex flex-col gap-2 basis-full bg-darkD-300 p-5">
+        <p>Штрих код: {product.code}</p>
+        <p>Дата изготовления: {product.date_1}</p>
+        <p>Дата просрочки: {product.date_2}</p>
+        <p>Добавил: {product.whoAdded}</p>
       </div>
 
       <div className="Product__toolbar flex gap-4 px-5 py-4 lg:rounded-b-lg">
