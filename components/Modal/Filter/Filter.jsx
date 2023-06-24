41bc7fa03ed1b1d034dbc3709f4a549cd40a9cc1
spandin <willstesi@gmail.com>
2023-06-24 21:42:19 +0300

    sort table func

---
+++ components/Modal/Filter/Filter.jsx
@@ -0,0 +1,61 @@
+import "./index.scss";
+
+export const Filter = ({ filterValue }) => {
+  return (
+    <div className="Filter flex flex-col gap-5 min-w-[275px]">
+      <h2 className="Filter px-[3px]">Фильтр таблицы</h2>
+
+      <div className="Filter__radio flex flex-col gap-1">
+        <div className="Filter__radio__item">
+          <label for="all">Все</label>
+          <input
+            type="radio"
+            name="sort"
+            value="all"
+            onClick={(e) => filterValue(e.target.value)}
+          />
+        </div>
+
+        <div className="Filter__radio__item">
+          <label for="products">Продукты</label>
+          <input
+            type="radio"
+            name="sort"
+            value="products"
+            onClick={(e) => filterValue(e.target.value)}
+          />
+        </div>
+
+        <div className="Filter__radio__item">
+          <label for="alcohol">Алкоголь</label>
+          <input
+            type="radio"
+            name="sort"
+            value="alcohol"
+            onClick={(e) => filterValue(e.target.value)}
+          />
+        </div>
+
+        <div className="Filter__radio__item">
+          <label for="chemistry">Химия</label>
+          <input
+            type="radio"
+            name="sort"
+            value="chemistry"
+            onClick={(e) => filterValue(e.target.value)}
+          />
+        </div>
+
+        <div className="Filter__radio__item">
+          <label for="other">Другое</label>
+          <input
+            type="radio"
+            name="sort"
+            value="other"
+            onClick={(e) => filterValue(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/Products/ProductsTable/ProductsTable.jsx
+++ components/Products/ProductsTable/ProductsTable.jsx
@@ -19,12 +19,17 @@ import { Modal } from "@/components/Modal/Modal";
 import { IcButton } from "@/components/Button/IcButton/IcButton";
 import { AddProduct } from "@/components/Modal/Products/AddProduct";
 import { Search } from "@/components/Modal/Search/Search";
+import { Filter } from "@/components/Modal/Filter/Filter";
 
 const ProductsTable = () => {
   const tableRef = useRef(null);
   const { user } = UserAuth();
+
   const [addModalActive, setAddModalActive] = useState(false);
   const [searchModalActive, setSearchModalActive] = useState(false);
+  const [filterModalActive, setFilterModalActive] = useState(false);
+
+  const [filterValues, setFilterValues] = useState("all");
   const [products, setProducts] = useState([]);
 
   const { onDownload } = useDownloadExcel({
@@ -36,12 +41,13 @@ const ProductsTable = () => {
   useEffect(() => {
     const q = query(collection(db, "products"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const pattern = /(\d{2})\.(\d{2})\.(\d{4})/; // для парсинга даты из "ru-RU" в IST
       let productsArr = [];
 
       querySnapshot.forEach((doc) => {
         productsArr.push({ ...doc.data(), id: doc.id });
       });
+
+      const pattern = /(\d{2})\.(\d{2})\.(\d{4})/; // для парсинга даты из "ru-RU" в IST
 
       productsArr.sort((a, b) => {
         return (
@@ -49,10 +55,30 @@ const ProductsTable = () => {
           new Date(b.date_2.replace(pattern, "$3-$2-$1"))
         );
       });
+
+      const findProductsByCategory = (category) =>
+        productsArr.filter((product) =>
+          Object.values(product).some(
+            (value) => ("" + value).indexOf(category) !== -1
+          )
+        );
+
+      if (filterValues === "products") {
+        return setProducts(findProductsByCategory("Продукты"));
+      } else if (filterValues === "alcohol") {
+        return setProducts(findProductsByCategory("Алкоголь"));
+      } else if (filterValues === "chemistry") {
+        return setProducts(findProductsByCategory("Химия"));
+      } else if (filterValues === "other") {
+        return setProducts(findProductsByCategory("Другое"));
+      }
+
+      console.log(filterValues);
+
       setProducts(productsArr);
     });
     return () => unsubscribe();
-  }, []);
+  }, [filterValues]);
 
   return (
     <div className="Products">
@@ -74,6 +100,7 @@ const ProductsTable = () => {
         <div className="flex gap-3 flex-row">
           <IcButton
             className="IcButtonA"
+            onClick={() => setFilterModalActive(true)}
             icon={<MdFilterList />}
             text="Фильтр"
           />
@@ -120,6 +147,10 @@ const ProductsTable = () => {
 
       <Modal active={addModalActive} setActive={setAddModalActive}>
         <AddProduct />
+      </Modal>
+
+      <Modal active={filterModalActive} setActive={setFilterModalActive}>
+        <Filter filterValue={setFilterValues} />
       </Modal>
 
       <Modal active={searchModalActive} setActive={setSearchModalActive}>
