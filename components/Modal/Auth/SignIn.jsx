66ceaef62aa5b42b13943069015dc24563530913
spandin <willstesi@gmail.com>
2023-07-26 19:59:43 +0300

    change on jsx

---
+++ components/Modal/Auth/SignIn.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "@/store/slices/userSlice";
+
+import { Form } from "./Form";
+
+const SignIn = () => {
+  const dispatch = useDispatch();
+  const [errorAuth, setErrorAuth] = useState("");
+
+  const onLogin = async (data, e) => {
+    const auth = getAuth();
+
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password).then(
+        ({ user }) => {
+          dispatch(
+            setUser({
+              email: user.email,
+              id: user.uid,
+              token: user?.accessToken,
+            })
+          );
+        }
+      );
+    } catch (e) {
+      console.log(`SignIn`, e.message);
+    }
+  };
+  return <Form tittle="Войти" onSubmit={onLogin} errorAuth={errorAuth} />;
+};
+
+export default SignIn;
