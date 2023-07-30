0bc1fa6d05de8ed3503b40d96b23a17ed710d8e9
spandin <willstesi@gmail.com>
2023-07-30 17:38:54 +0300

    Update page.jsx

--- app/add/page.jsx
+++ app/add/page.jsx
@@ -9,7 +9,6 @@ import { AddProductForm } from "../../components/Forms/Products/AddProductForm";
 import { TopBar } from "@/components/TopBar/TopBar";
 
 export default function AddProduct() {
-  console.log(`products`, products);
   return (
     <div className="AddProduct w-full">
       <TopBar tittle={"Добавить продукт"} />
