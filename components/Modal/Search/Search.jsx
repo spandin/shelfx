4b179a1fb2857d0a0506c5715fbec76f28a6d380
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2023-08-08 12:05:41 +0300

    Update Search.jsx

--- components/Modal/Search/Search.jsx
+++ components/Modal/Search/Search.jsx
@@ -53,7 +53,7 @@ export const Search = () => {
         {searchTerm.length >= 4
           ? searchResults.map((item) => (
               <div key={item.id} className="Search__item flex flex-col gap-0">
-                <Link href={`products/${item.id}`}>
+                <Link href={`posts/${item.id}`}>
                   {item.name} - {item.quantity} шт.{" "}
                 </Link>
               </div>
