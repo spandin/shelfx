cefa7b59deab651e2d9fe9e36433eda1fa8f420b
spandin <willstesi@gmail.com>
2023-08-22 21:37:21 +0300

    added search by name

--- components/Modal/Search/Search.jsx
+++ components/Modal/Search/Search.jsx
@@ -1,60 +1,80 @@
-import "./_index.scss";
+import './_index.scss';
 
-import Link from "next/link";
-import { useState, useEffect } from "react";
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
-import { db } from "@/lib/firebase";
-import { query, collection, onSnapshot } from "firebase/firestore";
+import { db } from '@/lib/firebase';
+import { query, collection, onSnapshot } from 'firebase/firestore';
 
 export const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchVariant, setSearchVariant] = useState('bar-code');
+  const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
-  const handleChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
   useEffect(() => {
-    const q = query(collection(db, "data"));
+    const q = query(collection(db, 'data'));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let productsArr = [];
+      let data = [];
 
       querySnapshot.forEach((doc) => {
-        productsArr.push({ ...doc.data(), id: doc.id });
+        data.push({ ...doc.data(), id: doc.id });
       });
 
-      const results = productsArr.filter((product) => {
-        // product.name.toLowerCase().includes(searchTerm); поиск по имени
-
-        return String(product.code)
-          .split("")
-          .reverse()
-          .join("")
-          .includes(searchTerm.split("").reverse().join(""));
+      const results = data.filter((product) => {
+        if (searchVariant === 'name') {
+          console.log(product.name.toLowerCase().includes(searchTerm));
+          return product.name.toLowerCase().includes(searchTerm);
+        } else {
+          return String(product.code)
+            .split('')
+            .reverse()
+            .join('')
+            .includes(searchTerm.split('').reverse().join(''));
+        }
       });
 
       setSearchResults(results);
     });
     return () => unsubscribe();
-  }, [searchTerm]);
+  }, [searchTerm, searchVariant]);
 
   return (
-    <div className="Search flex flex-col gap-5">
+    <div className="Search flex max-h-screen flex-col gap-5">
       <h3 className="Search px-[3px]">Поиск продукта</h3>
 
-      <input
-        type="number"
-        placeholder="Введите 4 любые цифры"
-        value={searchTerm}
-        onChange={handleChange}
-      />
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row gap-3 px-1 text-sm text-darkG-100">
+          <span
+            className={`${searchVariant === 'name' && 'text-gray-50'} hover:underline `}
+            onClick={() => setSearchVariant('name')}
+          >
+            по имени
+          </span>
+          <span
+            className={`${searchVariant === 'bar-code' && 'text-gray-50'} hover:underline`}
+            onClick={() => setSearchVariant('bar-code')}
+          >
+            по штрих коду
+          </span>
+        </div>
+
+        <input
+          className="min-w-[280px]"
+          type={searchVariant === 'name' ? 'text' : 'number'}
+          placeholder={
+            searchVariant === 'name' ? 'Введите название товара' : 'Введите 4 любые цифры товара'
+          }
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+        />
+      </div>
 
       <div className="flex flex-col gap-1">
         {searchTerm.length >= 4
           ? searchResults.map((item) => (
               <div key={item.id} className="Search__item flex flex-col gap-0">
                 <Link href={`posts/${item.id}`}>
-                  {item.name} - {item.quantity} шт.{" "}
+                  {item.name} - {item.quantity} шт.{' '}
                 </Link>
               </div>
             ))
