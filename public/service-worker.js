cfd2764682d6c64f4ef9bc9b35ef7025f9db96f5
spandin <willstesi@gmail.com>
2023-09-07 17:41:24 +0300

    pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,8 @@
-import { skipWaiting, clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
-import {
-  NetworkOnly,
-  NetworkFirst,
-  CacheFirst,
-  StaleWhileRevalidate,
-} from "workbox-strategies";
-import {
-  registerRoute,
-  setDefaultHandler,
-  setCatchHandler,
-} from "workbox-routing";
-import {
-  matchPrecache,
-  precacheAndRoute,
-  cleanupOutdatedCaches,
-} from "workbox-precaching";
+import { skipWaiting, clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { NetworkOnly, NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute, setDefaultHandler, setCatchHandler } from 'workbox-routing';
+import { matchPrecache, precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 
 skipWaiting();
 clientsClaim();
@@ -25,16 +12,16 @@ clientsClaim();
 const WB_MANIFEST = self.__WB_MANIFEST;
 // Precache fallback route and image
 WB_MANIFEST.push({
-  url: "/fallback",
-  revision: "1234567890",
+  url: '/fallback',
+  revision: '1234567890',
 });
 precacheAndRoute(WB_MANIFEST);
 
 cleanupOutdatedCaches();
 registerRoute(
-  "/",
+  '/',
   new CacheFirst({
-    cacheName: "start-url",
+    cacheName: 'start-url',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 1,
@@ -43,12 +30,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
   new CacheFirst({
-    cacheName: "google-fonts",
+    cacheName: 'google-fonts',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 4,
@@ -57,12 +44,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
   new StaleWhileRevalidate({
-    cacheName: "static-font-assets",
+    cacheName: 'static-font-assets',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 4,
@@ -71,13 +58,13 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 // disable image cache, so we could observe the placeholder image when offline
 registerRoute(
   /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
   new NetworkOnly({
-    cacheName: "static-image-assets",
+    cacheName: 'static-image-assets',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 64,
@@ -86,12 +73,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /\.(?:js)$/i,
-  new StaleWhileRevalidate({
-    cacheName: "static-js-assets",
+  new CacheFirst({
+    cacheName: 'static-js-assets',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 32,
@@ -100,12 +87,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /\.(?:css|less)$/i,
   new StaleWhileRevalidate({
-    cacheName: "static-style-assets",
+    cacheName: 'static-style-assets',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 32,
@@ -114,12 +101,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /\.(?:json|xml|csv)$/i,
   new NetworkFirst({
-    cacheName: "static-data-assets",
+    cacheName: 'static-data-assets',
     plugins: [
       new ExpirationPlugin({
         maxEntries: 32,
@@ -128,12 +115,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /\/api\/.*$/i,
   new NetworkFirst({
-    cacheName: "apis",
+    cacheName: 'apis',
     networkTimeoutSeconds: 10,
     plugins: [
       new ExpirationPlugin({
@@ -143,12 +130,12 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 registerRoute(
   /.*/i,
-  new NetworkFirst({
-    cacheName: "others",
+  new CacheFirst({
+    cacheName: 'others',
     networkTimeoutSeconds: 10,
     plugins: [
       new ExpirationPlugin({
@@ -158,7 +145,7 @@ registerRoute(
       }),
     ],
   }),
-  "GET"
+  'GET',
 );
 
 // following lines gives you control of the offline fallback strategies
@@ -179,17 +166,17 @@ setCatchHandler(({ event }) => {
   // One approach would be to use request.destination, see
   // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
   switch (event.request.destination) {
-    case "document":
+    case 'document':
       // If using precached URLs:
-      return matchPrecache("/fallback");
+      return matchPrecache('/fallback');
       // return caches.match('/fallback')
       break;
-    case "image":
+    case 'image':
       // If using precached URLs:
-      return matchPrecache("/static/images/fallback.png");
+      return matchPrecache('/static/images/fallback.png');
       // return caches.match('/static/images/fallback.png')
       break;
-    case "font":
+    case 'font':
     // If using precached URLs:
     // return matchPrecache(FALLBACK_FONT_URL);
     // return caches.match('/static/fonts/fallback.otf')
