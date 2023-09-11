57c74e5ebfc95e719ff24286303e57f88f50ac31
spandin <willstesi@gmail.com>
2023-09-11 22:12:02 +0300

    Update DeletePost.jsx

--- components/Forms/Posts/DeletePost.jsx
+++ components/Forms/Posts/DeletePost.jsx
@@ -1,30 +1,34 @@
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-import { db } from '@/lib/firebase';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from "@/lib/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
 
-import { IcButton } from '@/components/Button/IcButton/IcButton';
+import { IcButton } from "@/components/Button/IcButton/IcButton";
 
 const DeletePost = ({ name, id }) => {
   const router = useRouter();
 
   const deletePost = async () => {
     try {
-      await deleteDoc(doc(db, 'data', id));
+      await deleteDoc(doc(db, "data", id));
       router.back();
     } catch (e) {
-      console.log('Delete Post: ' + e.message);
+      console.log("Delete Post: " + e.message);
     }
   };
 
   return (
     <div className="flex flex-col justify-center gap-8">
       <div className="flex flex-col gap-2 px-[3px]">
-        <p>Вы действительно хотите удалить?</p>
+        <h3>Вы действительно хотите удалить?</h3>
         <p className="text-sm text-darkG-100">&quot;{name}&quot;</p>
       </div>
 
-      <IcButton className="px-3 text-sm" text="Удалить" onClick={() => deletePost()} />
+      <IcButton
+        className="px-3 text-sm"
+        text="Удалить"
+        onClick={() => deletePost()}
+      />
     </div>
   );
 };
